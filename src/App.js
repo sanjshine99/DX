397eb3b6784e57d3ee414d1e1034a57bb14eaf48
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-09-13 02:18:45 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
-import About from './components/about/About';
-import Blog from './components/blog/Blog';
-import Stormbreaker19 from './components/products/Stormbreaker19';
-import Stormbreaker21 from './components/products/Stormbreaker21';
-import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+
+const About = lazy(() => import('./components/about/About'));
+const Blog = lazy(() => import('./components/blog/Blog'));
+const Stormbreaker19 = lazy(() => import('./components/products/Stormbreaker19'));
+const Stormbreaker21 = lazy(() => import('./components/products/Stormbreaker21'));
+const Contact = lazy(() => import('./components/contact/Contact'));
 
 function App() {
   return (
     <div className="App">
-       <HashRouter>
-          <Header />
+      <HashRouter>
+        <Header />
+        <Suspense fallback={<div>loading</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
@@ -23,8 +25,9 @@ function App() {
             <Route path="/stormbreaker21" element={<Stormbreaker21 />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-          <Footer />
-        </HashRouter>
+        </Suspense>
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
